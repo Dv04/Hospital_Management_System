4dc38167518c1bebf668c05b438dab9166ed5518
MysteryManav <madhurbprajapati2@gmail.com>
2023-08-26 09:56:35 +0530

    login and register form setup

--- forms.py
+++ forms.py
@@ -9,4 +9,17 @@ class PatientDetailsForm(FlaskForm):
     pass
 
 class LoginUserForm(FlaskForm):
-    pass
+    email = StringField("Email", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    role = SelectField("Role", choices=[("doctor", "Doctor"), ("reception", "Reception"), ("staff", "Staff"), ("patient", "Patient"), ("pharmacy", "Pharmacy")])
+    submit = SubmitField("Sign In")
+
+class RegisterUserForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    role = SelectField("Role", choices=[("doctor", "Doctor"), ("reception", "Reception"), ("staff", "Staff"), ("patient", "Patient"), ("pharmacy", "Pharmacy")])
+    phone_no = IntegerField("Phone No.", validators=[DataRequired()])
+    gender = SelectField("Gender", validators=[DataRequired()], choices=[("male", "Male"), ("female", "Female"), ("other", "Other")])
+    address = TextAreaField("Address", validators=[DataRequired()])
+    submit = SubmitField("Register")
